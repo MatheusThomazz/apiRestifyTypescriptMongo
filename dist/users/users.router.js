b6e6fc3bf6e279308c3bde28a7f2e5775167d68c
Matheus Thomaz <matheusthomazt@gmail.com>
2020-05-22 00:11:18 -0300

    error.handler criado

--- dist/users/users.router.js
+++ dist/users/users.router.js
@@ -2,35 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.usersRouter = void 0;
 const router_1 = require("../common/router");
+const restify_errors_1 = require("restify-errors");
 const users_model_1 = require("./users.model");
 class UsersRouter extends router_1.Router {
+    constructor() {
+        super();
+        this.on('beforeRender', document => {
+            document.password = undefined;
+            //delete document.password
+        });
+    }
     appyRoutes(application) {
         application.get('/users', (req, resp, next) => {
-            users_model_1.User.find().then(users => {
-                resp.json(users);
-                return next();
-            });
+            users_model_1.User.find().then(this.render(resp, next));
         });
         application.get('/users/:id', (req, resp, next) => {
-            users_model_1.User.findById(req.params.id).then(user => {
-                console.log(user);
-                if (user) {
-                    resp.json(user);
-                    return next();
-                }
-                else {
-                    resp.send(404);
-                    return next();
-                }
-            });
+            users_model_1.User.findById(req.params.id)
+                .then(this.render(resp, next))
+                .catch(next);
         });
         application.post('/users', (req, resp, next) => {
             let user = new users_model_1.User(req.body);
-            user.save().then(user => {
-                user.password = undefined;
-                resp.json(user);
-                return next();
-            });
+            user.save().then(this.render(resp, next))
+                .catch(next);
         });
         application.put('/users/:id', (req, resp, next) => {
             const options = { overwrite: true };
@@ -40,23 +34,16 @@ class UsersRouter extends router_1.Router {
                     return users_model_1.User.findById(req.params.id).exec();
                 }
                 else {
-                    resp.send(404);
+                    throw new restify_errors_1.NotFoundError('Documento nao encontrado');
                 }
-            }).then(user => {
-                resp.json(user);
-                return next();
-            });
+            }).then(this.render(resp, next))
+                .catch(next);
         });
         application.patch('/users/:id', (req, resp, next) => {
             const options = { new: true };
-            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options).then(user => {
-                if (user) {
-                    resp.json(user);
-                    return next();
-                }
-                resp.send(400);
-                return next();
-            });
+            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options)
+                .then(this.render(resp, next))
+                .catch(next);
         });
         application.del('/users/:id', (req, resp, next) => {
             users_model_1.User.remove({ _id: req.params.id }).exec().then((cmdResult) => {
@@ -64,10 +51,11 @@ class UsersRouter extends router_1.Router {
                     resp.send(204);
                 }
                 else {
-                    resp.send(404);
+                    throw new restify_errors_1.NotFoundError('Documento nao encontrado');
                 }
                 return next();
-            });
+            })
+                .catch(next);
         });
     }
 }
--- users/users.router.ts
+++ users/users.router.ts
@@ -1,41 +1,34 @@
 import { Router } from '../common/router';
 import * as restify from 'restify';
+import {NotFoundError} from 'restify-errors'
 import { User } from './users.model';
 
 
 class UsersRouter extends Router {
+
+    constructor(){
+      super()
+      this.on('beforeRender', document =>{
+          document.password = undefined
+          //delete document.password
+      })
+    }
     appyRoutes(application: restify.Server){
         
         application.get('/users', (req, resp, next) =>{
-            User.find().then(users =>{
-                resp.json(users)
-                return next()
-            })
-
+            User.find().then(this.render(resp, next))
         })
        
         application.get('/users/:id',(req, resp, next) =>{
-            User.findById(req.params.id).then(user =>{
-                console.log(user)
-                if(user){
-                    resp.json(user)
-                    return next()
-                }else{
-                    resp.send(404)
-                    return next()
-                }
-                
-            })
+            User.findById(req.params.id)
+                .then(this.render(resp, next))
+                .catch(next)
         })
 
         application.post('/users',(req, resp, next) =>{
             let user = new User(req.body)
-            user.save().then(user =>{
-                user.password = undefined
-                resp.json(user)
-                return next()
-            })
-            
+            user.save().then(this.render(resp, next))
+                .catch(next)
         })
 
         application.put('/users/:id', (req, resp, next) =>{
@@ -45,27 +38,19 @@ class UsersRouter extends Router {
                 if(result.n){
                     return User.findById(req.params.id).exec()
                 }else{
-                    resp.send(404)
+                    throw new NotFoundError('Documento nao encontrado')
 
                 }
-            }).then(user =>{
-                resp.json(user)
-                return next()
-
-            })
+            }).then(this.render(resp, next))
+              .catch(next)
 
         })
 
         application.patch('/users/:id', (req, resp, next) =>{
             const options = {new : true}
-            User.findByIdAndUpdate(req.params.id, req.body, options).then(user =>{
-                if(user){
-                    resp.json(user)
-                    return next()
-                }
-                resp.send(400)
-                return next()
-            }) 
+            User.findByIdAndUpdate(req.params.id, req.body, options)
+                .then(this.render(resp, next)) 
+                .catch(next)
              
         })
 
@@ -74,10 +59,11 @@ class UsersRouter extends Router {
                 if(cmdResult.result.n){
                     resp.send(204)
                 }else{
-                    resp.send(404)
+                    throw new NotFoundError('Documento nao encontrado')
                 }
                 return next()
             })
+            .catch(next)
         })
 
 
